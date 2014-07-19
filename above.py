5212e41de027566676e40340c2e3ddd786177517
chenjun <chenjun-616@163.com>
2014-07-19 18:34:23 +0800

    test1

---
+++ above.py
@@ -0,0 +1,5 @@
+def above_freezing(t):
+     return t>0
+
+def above_freezing2(t):
+     return t>0
---
+++ test.py
@@ -0,0 +1,11 @@
+import nose
+from above import *
+
+def test_freezing():
+    assert above_freezing(32) == 1
+
+def test_freezing2():
+    assert above_freezing(32) == 1
+
+if __name__ == '__main__':
+    nose.runmodule()
