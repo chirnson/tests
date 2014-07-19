39b0aeb2b41e63f4571932496444c93ef8c45d8f
chenjun <chenjun-616@163.com>
2014-07-19 18:54:41 +0800

    test3

--- test.py
+++ test.py
@@ -7,5 +7,8 @@ def test_freezing():
 def test_freezing2():
     assert above_freezing(32) == 1
 
+def test_freezing3():
+    assert above_freezing(32) == 1
+
 if __name__ == '__main__':
     nose.runmodule()
